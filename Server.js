fd0afa8cafa5d76461017062f9e14fbbf7b01386
BOT Panzer <panitroll33@example.com>
2018-12-26 19:57:24 +0100

    Port

--- Server.js
+++ Server.js
@@ -5,6 +5,10 @@ let xp = require("./commands/Xp/xp.json");
 var fs = require('fs');
 var money = require('discord-money');
 const token = process.env.token;
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 
 
 client.on("ready", () => {
