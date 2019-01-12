5107ab3b247194bbf3eb942b77c4393ef2a85b8c
BOT Panzer <panitroll33@example.com>
2019-01-12 18:33:02 +0100

    Añadido hotel al comando shop

--- commands/Dinero/shop.js
+++ commands/Dinero/shop.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
       .setTitle("🕳Tienda Divot =")
       .setDescription("Comando para comprar : " + prfix + "buy <objeto>`")
       .addField("Banco | XPmulti :", `${i.money}$ | x${xp[message.author.id].xpmul}`)
-      .addField("🎁Objetos =", "- ***XPx2*** : Te da el **doble de XP por 1 dia** al hablar **(Acumulable)**.\n**(750$)**\n- ***1lvl*** : Compras **1 lvl**.\n**(350$)**\n- ***5lvls*** : Compras **5 lvls**.\n**(1600$)**\n" + buyban)
+      .addField("🎁Objetos =", "- ***XPx2*** : Te da el **doble de XP por 1 dia** al hablar **(Acumulable)**.\n**(750$)**\n- ***1lvl*** : Compras **1 lvl**.\n**(350$)**\n- ***5lvls*** : Compras **5 lvls**.\n**(1600$)**\n- ***hotel*** : Compras **una sala privada** para **hacer lo que quieras** por **5 dias**.\n**(350$)**\n" + buyban)
       .setColor('RANDOM');
   
     message.channel.send(botembed);
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
       .setTitle("🕳Tienda Divot =")
       .setDescription("Comando para comprar : " + prfix + "buy <objeto>`")
       .addField("Banco | XPmulti :", `${i.money}$ | x${xp[message.author.id].xpmul}`)
-      .addField("🎁Objetos =", "- ***XPx2*** : Te da el **doble de XP por 1 dia** al hablar **(Acumulable)**.\n**(750$)**\n- ***1lvl*** : Compras **1 lvl**.\n**(350$)**\n- ***5lvls*** : Compras **5 lvls**.\n**(1600$)**\n" + buyban)
+      .addField("🎁Objetos =", "- ***XPx2*** : Te da el **doble de XP por 1 dia** al hablar **(Acumulable)**.\n**(750$)**\n- ***1lvl*** : Compras **1 lvl**.\n**(350$)**\n- ***5lvls*** : Compras **5 lvls**.\n**(1600$)**\n- ***hotel*** : Compras **una sala privada** para **hacer lo que quieras** por **5 dias**.\n**(350$)**\n" + buyban)
       .setColor('RANDOM');
   
     message.channel.send(botembed);
