433907c9f872422e143e73da3f3bca90897b2d47
BOT Panzer <panitroll33@example.com>
2019-01-12 18:41:53 +0100

    Fix syrup shop

--- commands/Dinero/shop.js
+++ commands/Dinero/shop.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     let prfix = "`" + serverconfig[message.guild.id].prefix
     let botembed = new Discord.RichEmbed()
       .setThumbnail(bicon)
-      .setTitle("🕳Tienda Divot =")
+      .setTitle("🥞Tienda Syrup =")
       .setDescription("Comando para comprar : " + prfix + "buy <objeto>`")
       .addField("Banco | XPmulti :", `${i.money}$ | x${xp[message.author.id].xpmul}`)
       .addField("🎁Objetos =", "- ***XPx2*** : Te da el **doble de XP por 1 dia** al hablar **(Acumulable)**.\n**(750$)**\n- ***1lvl*** : Compras **1 lvl**.\n**(350$)**\n- ***5lvls*** : Compras **5 lvls**.\n**(1600$)**\n- ***hotel*** : Compras **una sala privada** para **hacer lo que quieras** por **5 dias**.\n**(350$)**\n" + buyban)
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
     let prfix = "`" + serverconfig[message.guild.id].prefix
     let botembed = new Discord.RichEmbed()
       .setThumbnail(bicon)
-      .setTitle("🕳Tienda Divot =")
+      .setTitle("🥞Tienda Syrup =")
       .setDescription("Comando para comprar : " + prfix + "buy <objeto>`")
       .addField("Banco | XPmulti :", `${i.money}$ | x${xp[message.author.id].xpmul}`)
       .addField("🎁Objetos =", "- ***XPx2*** : Te da el **doble de XP por 1 dia** al hablar **(Acumulable)**.\n**(750$)**\n- ***1lvl*** : Compras **1 lvl**.\n**(350$)**\n- ***5lvls*** : Compras **5 lvls**.\n**(1600$)**\n- ***hotel*** : Compras **una sala privada** para **hacer lo que quieras** por **5 dias**.\n**(350$)**\n" + buyban)
