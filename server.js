17be8cc5f6b6891f0529d06aa39926ac6887b5b9
BOT Panzer <panitroll33@example.com>
2019-01-13 13:25:59 +0100

    fkng token

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 var money = require('discord-money');
 var fs = require('fs');
 let serverconfig = require("./commands/serverconfig.json");
 let xp = require("./commands/Xp/xp.json");
-const token = "NTI4ODk4OTYzNjY4MDc0NDk2.DxugJA.KNuWmDruf_jTmUGTIN-oRnR-wCM";
+const token = process.env.token;
 let userdata = require("./commands/user.json");
 function doMagic() {
   var rand = ['el nuevo video de Pewd', 'memes en reddit', 'bailes rusos', 'tutoriales de FdFlavia', 'el pack de Carbo'];
